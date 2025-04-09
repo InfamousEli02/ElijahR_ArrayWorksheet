4c0739e8ba72c6088d86ec82731ae143f270709c
InfamousEli02 <ejreed328@gmail.com>
2025-04-09 15:45:17 -0400

    added and completed all parts of worksheet

---
+++ arrayWorksheet.js
@@ -0,0 +1,88 @@
+//Part 1!
+let fruits = ["apple", "banana", "cherry"];
+console.log(`the value at index 1 of the fruits array is`, fruits[1]);
+console.log(`the total number of items in the fruits array is`, fruits.length);
+
+//Part 2!
+let addFruit = fruits.push("orange");
+console.log(fruits);
+
+let removedFruit = fruits.pop();
+console.log(fruits);
+console.log(`removed fruit:`, removedFruit);
+
+let addFruit2 = fruits.push("grape", "watermelon");
+let removedFruit2 = fruits.pop();
+console.log(fruits);
+console.log(`updated length`, fruits.length);
+
+//Part 3!
+for (let index = 0; index < fruits.length; index++ ){
+    console.log(fruits[index]);
+}
+
+for (let index = 0; index < fruits.length; index++ ){
+    console.log(fruits[index].toUpperCase());
+}
+
+let count = 0;
+for (let i = 0; i < fruits.length; i++ ){
+    if( fruits[i] === "apple" ){
+        count += 1;
+    }
+console.log(`${fruits[0]} appears ` + count + ` time(s)`);
+}
+
+//Part 4!
+let cart = [];
+let addGrocery = cart.push("Bread", "Milk", "Eggs");
+console.log(cart);
+
+let removeGrocery = cart.pop();
+console.log(cart);
+console.log(`length of the cart is`, cart.length);
+
+let numbers = [1, 2, 3, 4, 5];
+//start from end of array, go backwards
+for (let number = numbers.length - 1; number >= 0; number--){
+    console.log(numbers[number]);
+}
+
+//Part 5!
+let array1 = ["a", "b", "c", "d"];
+let array2= ["red", "green", "blue"];
+
+for(let i = 0; i < 5; i++){
+    if( array1.length > array2.length){
+        console.log(`array1 has the greater length`);
+        break;
+    }else{
+        console.log(`array2 has the greater length`);
+    }
+}
+
+//Part 6!
+const array = [10, 20, 30, 40, 50, 60];
+let evenArray = [];
+
+for(let i = 1; i <= array.length; i+=2){
+    const num = array[i];
+    if( num % 2 === 0){
+        // evenArray.push(num);  
+        console.log(num, `is even`);
+    }
+    
+}
+
+//Part 7!
+let studentNames = ["Alice", "Bob", "Charlie"];
+let subjectNames = ["Math", "Science"];
+
+for(let i = 0; i < studentNames.length; i++){
+    for(let subject = 0; subject < subjectNames.length; subject++){
+        console.log(studentNames[i], `-`, subjectNames[subject]);
+    }
+}
+//A Nested for loop works by checking if the outer for loop's conditions are still true first
+//before going to the inner for loop. The inner loop executes an action inside the outer loop
+//each time the outer loop does.
